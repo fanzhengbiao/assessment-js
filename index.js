017b7cef441f160029a751249b2f314d332f4371
樊正彪 <fanzhengbiao@wisers.com>
2022-06-17 17:43:31 +0800

    Add files via upload
    1.修改eslint报错
    2.从新更新setDefault方法
    3.因为时间，写完没有运行test，今天一运行，过了5个case，两个没过，所以更新一下内容
    4.不论结果是什么，态度先摆正了。

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const utils = require( TEST_LOCATION ); // jest global
 
 /**
  * Example problem with existing solution and passing test.
@@ -8,47 +7,45 @@ const utils = require( TEST_LOCATION ); // jest global
 exports.example = () => 'hello world';
 
 
-
-
 /**
  * @param {[]} objectArray 给定的对象数组 
  * const arraydata = [{ key: 'a', value: 'c', name: 'f' }, { key: 'b', value: 'd', name: 'd' }]
  * @param {string} stripProperty 需要剥离的属性
- * @test excludeByProperty(['name'], arraydata) => [{ key: 'a', value: 'c' }， { key: 'b', value: 'd' }]
+ * @test excludeByProperty(['name'], arraydata) =>
+ * [{ key: 'a', value: 'c' }，{ key: 'b', value: 'd' }]
  */
 exports.stripPrivateProperties = (stripProperty, objectArray) => {
-    try {
-        return objectArray.reduce((ret, item) => {
-            for (let objkey in item) {
-                if (stripProperty.includes(objkey)) {
-                    delete item[objkey];
-                }
-            }
-            ret.push(item);
-            return ret;
-        }, []);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    return objectArray.reduce((ret, item) => {
+      for (let objkey in item) {
+        if (stripProperty.includes(objkey)) {
+          delete item[objkey];
+        }
+      }
+      ret.push(item);
+      return ret;
+    }, []);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 
 /**
- * 
- * @param {[]} objectArray 给定的对象数组 
+ * @param {[]} objectArray 给定的对象数组
  * const arraydata = [{ key: 'a', value: 'c' }, { key: 'b', name: 'd' }]
  * @param {string} excludeProperty 需要排除的属性
  * @test excludeByProperty(arraydata, 'name') => [{ key: 'a', value: 'c' }]
  */
 exports.excludeByProperty = (excludeProperty, objectArray) => {
-    try {
-        if (Array.isArray(objectArray)) {
-            return objectArray.filter(item => !item[excludeProperty]);
-        }
-        return [];
-    } catch (err) {
-        console(err);
+  try {
+    if (Array.isArray(objectArray)) {
+      return objectArray.filter(item => !item[excludeProperty]);
     }
+    return [];
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 
@@ -56,13 +53,18 @@ exports.excludeByProperty = (excludeProperty, objectArray) => {
  * 利用双层嵌套循环
  */
 exports.sumDeep = (data) => {
-    return data.map(item => {
-        const sumDeep = item.objects.reduce((res, obj) => {
-            res += obj.val;
-            return res;
-        }, 0);
-        return { objects: sumDeep };
+  try {
+    return data.map((item) => {
+      const sumDeep = item.objects.reduce((res, obj) => {
+        let temp = { ...res };
+        temp += obj.val;
+        return temp;
+      }, 0);
+      return { objects: sumDeep };
     });
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 
@@ -71,34 +73,45 @@ exports.sumDeep = (data) => {
  * @param {[]} statusArray 状态集合
  */
 exports.applyStatusColor = (dataSource, statusArray) => {
-    try {
-        return statusArray.reduce((ret, item) => {
-            const findKey = Object.keys(dataSource).find(key => dataSource[key].includes(item['status']));
-            if (findKey) {
-                ret.push({
-                  status: item['status'],
-                  color: findKey,
-                })
-            }
-           return ret;
-        }, []);
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    return statusArray.reduce((ret, item) => {
+      const findKey = Object.keys(dataSource).find(key => dataSource[key].includes(item.status));
+      if (findKey) {
+        ret.push({
+          status: item.status,
+          color: findKey,
+        });
+      }
+      return ret;
+    }, []);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
 
 
 /**
  * 一等函数
  */
-exports.createGreeting = greeting => person => `${greeting} ${person}`;
+exports.createGreeting = (fn, greeting) => person => `${greeting} ${person}`;
 
 
 /**
- * 这道题我可能取巧了，题目没有要求对象里面key的顺序，就一句话就可以实现
- * 参数有交集不更新，并集更新，直接assign就完事了
+ * 学习了一下一等函数的写法，重新完善了一下题目
  */
-exports.setDefaults = ((obj = {}) => Object.assign({}, { subscribed: true }, obj));
+exports.setDefaults = (defaultObj) => {
+  const fn = (obj) => {
+    const tmpObj = { ...obj };
+    const tmpDatas = Object.keys(defaultObj).reduce((ret, key) => {
+      if (!tmpObj.prototypes.hasOwnProperty.call(key)) {
+        tmpObj[key] = defaultObj[key];
+      }
+      return tmpObj;
+    }, {});
+    return tmpDatas;
+  };
+  return fn;
+};
 
 
 /**
@@ -108,19 +121,14 @@ exports.setDefaults = ((obj = {}) => Object.assign({}, { subscribed: true }, obj
  * @return { company, status, user }
  */
 exports.fetchUserByNameAndUsersCompany = async (userName, services) => {
-    const status = await services.fetchStatus();
-    const usersData = await services.fetchUsers();
-    const user = usersData.find(user => user.name === userName);
-    const company = await new Promise(resolve => {
-        if (user && user.companyId) {
-            resolve(user.companyId);
-        }
-    }).then(comId => services.fetchCompanyById(comId));
-    
-    return {
-       company,
-       status,
-       user,
+  const status = await services.fetchStatus();
+  const usersData = await services.fetchUsers();
+  const user = usersData.find(userDt => userDt.name === userName);
+  const company = await new Promise((resolve) => {
+    if (user && user.companyId) {
+      resolve(user.companyId);
     }
-}
+  }).then(comId => services.fetchCompanyById(comId));
 
+  return { company, status, user };
+};
